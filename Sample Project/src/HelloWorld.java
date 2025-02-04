78e076ad6adf71e3cb21ef4e3ac50e2be30b26e9
Yves Lyndon M. Bermudez <yveslyndon.bermudez@benilde.edu.ph>
2025-02-04 22:17:34 +0800

    Initial commit

---
+++ Sample Project/src/HelloWorld.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Print Hello World
+        //System.out.println("Hello World!");
+
+        System.out.println("Enter your name: ");
+
+        // Scan User Input
+       // Scanner inputScanner = new Scanner(System.in);
+       // String name = inputScanner.nextLine();
+
+        //System.out.println("Hello " + name + "!");
+    }
+}
